6a779de2ba5a31cef2aa96ff128136b9280e38fa
dabba <dabbaghi.im@gmail.com>
2025-03-03 19:46:54 +0100

    Init project

---
+++ src/main/java/ch/be/datahackdays2025/power_outage/PowerOutageApplication.java
@@ -0,0 +1,13 @@
+package ch.be.datahackdays2025.power_outage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerOutageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerOutageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/be/datahackdays2025/power_outage/PowerOutageApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.be.datahackdays2025.power_outage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerOutageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
