6d1e7a8700c6a071e322a85012173fa37c600d79
dabba <dabbaghi.im@gmail.com>
2025-03-13 21:59:06 +0100

    spring application starts and connects to the db

--- src/main/java/ch/be/datahackdays2025/poweroutage/PowerOutageApplication.java
+++ src/main/java/ch/be/datahackdays2025/poweroutage/PowerOutageApplication.java
@@ -2,8 +2,9 @@ package ch.be.datahackdays2025.poweroutage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PowerOutageApplication {
 
 	public static void main(String[] args) {
