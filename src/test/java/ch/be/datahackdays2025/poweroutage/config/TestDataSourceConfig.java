a162a70ea5ff7c6721273413d2aa33ae598d328a
dabba <dabbaghi.im@gmail.com>
2025-03-30 22:38:47 +0200

    remove empty test

--- src/test/java/ch/be/datahackdays2025/poweroutage/config/TestDataSourceConfig.java
+++ src/test/java/ch/be/datahackdays2025/poweroutage/config/TestDataSourceConfig.java
@@ -2,18 +2,21 @@ package ch.be.datahackdays2025.poweroutage.config;
 
 
 // TestDataSourceConfig.java
+
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
-@Configuration
+@TestConfiguration
 @Profile("test")
 public class TestDataSourceConfig {
 
     @Bean
+    @Primary
     public DataSource dataSource() {
         return DataSourceBuilder.create()
                 .url("jdbc:h2:mem:testdb")
